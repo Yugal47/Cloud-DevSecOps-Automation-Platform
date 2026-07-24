32aeb35c4b8b124599003266b1c117b6591b4e7a
Yugal47 <yugaltiwari47@gmail.com>
2026-07-24 10:54:40 +0530

    Made changes in SonarQube userdata

--- terraform/userdata/SonarQube.sh
+++ terraform/userdata/SonarQube.sh
@@ -1,15 +1,15 @@
 #!/bin/bash
-sudo apt update && sudo apt upgrade -y
+apt update && sudo apt upgrade -y
 #install docker
 
-sudo apt install -y apt-transport-https ca-certificates curl software-properties-common
+apt install -y apt-transport-https ca-certificates curl software-properties-common
 curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo apt-key add -
-sudo add-apt-repository "deb [arch=amd64] https://download.docker.com/linux/ubuntu $(lsb_release -cs) stable"
-sudo apt update
-sudo apt install -y docker-ce
-sudo chmod 666 /var/run/docker.sock
+add-apt-repository "deb [arch=amd64] https://download.docker.com/linux/ubuntu $(lsb_release -cs) stable"
+apt update
+apt install -y docker-ce
+chmod 666 /var/run/docker.sock
 
-sudo systemctl enable docker
-sudo systemctl start docker
+systemctl enable docker
+systemctl start docker
 
 docker run -d --name sonar -p 9000:9000 sonarqube:lts-community
